4417560d6d77bd5b44a08b214e658cde5e9bfd9b
Gabriel Henrique da Silva <gabriel04gh1.gh@gmail.com>
2021-04-26 14:12:10 -0300

    Teste (adição de notas e localstorage)

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@ function load() {
     const container = document.querySelector('.container')
     const imgInput = document.querySelectorAll('.imgInput')
     const form = container.querySelector('form')
+    const outTasks = container.querySelector('.outTasks')
     const allTasks = []
+
     function newNote() {
         moreOptions()
         openForm()
@@ -18,18 +20,33 @@ function load() {
 
         const task = form.querySelector('#task')
         const text = form.querySelector('#text')
-        
+
         createTasks(task, text)
     }
-  function createTasks(task, text) {
+
+    function createTasks(task, text) {
         if (task.value || text.value) allTasks.push({ title: task.value, text: text.value })
         task.value = text.value = ``
+
+        setTasks(outTasks, allTasks)
+
+        localStorage.setItem(`task${allTasks.length}`, JSON.stringify(allTasks[allTasks.length - 1]))
+    }
+
+    function setTasks(body, value, onload) {
+        if (onload) {
+            const obj = []
+            
+            for (let values of value) {
+                obj.push(JSON.parse(eval(`localStorage.${values}`)))
+            }
+
+            value = obj
+        }
+
+        body.innerHTML = ``
         
-        const outTasks = container.querySelector('.outTasks')
-        
-        outTasks.innerHTML = ``
-        
-        for (let task of allTasks) {
+        for (let task of value) {
             outTasks.innerHTML += `
                 <div class='tasks'>
                     <h3>${task.title}</h3>
@@ -38,13 +55,13 @@ function load() {
             `
         }
 
-        localStorage.setItem(`task${allTasks.length}`, JSON.stringify(allTasks[allTasks.length - 1]))
+
     }
-  
 
     function moreOptions() {
         document.querySelector('nav').classList.toggle('nav')
         imgInput[4].classList.toggle('clicked')
+        
         if (imgInput[4].getAttribute('src') === './assets/icon/more-options.svg') {
             imgInput[4].setAttribute('src', './assets/icon/cancel.svg')
         } else {
@@ -52,11 +69,25 @@ function load() {
         }
     }
 
+    function loadTasksStorage() {
+        const len = localStorage.length
+        
+        if (!len) return
+
+        const keys = Object.keys(localStorage);
+        
+        setTasks(outTasks, keys, true)
+    }
+
+    loadTasksStorage()
+
     form.addEventListener('submit', newTask)
+
     document.addEventListener('click', (e) => {
         if (e.target === imgInput[0]) newNote()
         if (e.target === imgInput[4]) moreOptions()
     })
+
 }
 
 load()
