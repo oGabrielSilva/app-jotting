0a366c5eeb7c3059e66debcff0c6e2a4fe898f15
Gabriel Henrique da Silva <gabriel04gh1.gh@gmail.com>
2021-04-25 21:18:12 -0300

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+function load() {
+    const container = document.querySelector('.container')
+    const imgInput = document.querySelectorAll('.imgInput')
+    console.log(imgInput)
+
+    function newNote() {
+        container.querySelector('form').classList.toggle('form')
+    }
+
+    function moreOptions() {
+        document.querySelector('nav').classList.toggle('nav')
+    }
+
+    document.addEventListener('click', (e) => {
+        if (e.target === imgInput[0]) newNote()
+        if (e.target === imgInput[5]) moreOptions()
+        if (e.target === imgInput[2]) newNote()
+    })
+}
+
+load()
