bab9750cfa544e7275b9c6aa6cebe4eb5b7a532c
Gabriel Henrique da Silva <gabriel04gh1.gh@gmail.com>
2021-04-29 19:21:20 -0300

    Visualização de notas na tela

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,44 @@
 function load() {
-    const container = document.querySelector('.container')
-    const imgInput = document.querySelectorAll('.imgInput')
-    const form = container.querySelector('form')
-    const liInput = document.querySelectorAll('li')
-    const outTasks = container.querySelector('.outTasks')
-    const allTasks = []
+    //constantes e variáveis
 
-    console.log(liInput)
+    const container = document.querySelector('.container');
+    const imgInput = document.querySelectorAll('.imgInput');
+    const form = container.querySelector('form');
+    const liInput = document.querySelectorAll('li');
+    const outTasks = container.querySelector('.outTasks');
+    const allTasks = [];
+    let tasks;
+
+    //constantes e variáveis
 
     function newNote() {
         moreOptions()
         openForm()
     }
 
+    // formulário
+
     function openForm() {
         container.querySelector('form').classList.toggle('form')
     }
 
+    // formulário
+
+
+
+    //criar uma tarefa nova
+
+    function newNote() {
+        moreOptions()
+        openForm()
+    };
+
     function newTask(e) {
-        e.preventDefault()
+        e.preventDefault();
         openForm()
 
-        const task = form.querySelector('#task')
-        const text = form.querySelector('#text')
+        const task = form.querySelector('#task');
+        const text = form.querySelector('#text');
 
         createTasks(task, text)
     }
@@ -48,10 +64,26 @@ function load() {
                     <p>${task.text}</p>
                 </div>
             `
-        }
+        };
 
-
+        setEventListener()
     }
+
+    // criar uma tarefa nova
+
+    //Storage
+
+    function loadTasksStorage() {
+        const len = localStorage.length;
+
+        if (!len) return;
+
+        const keys = Object.keys(localStorage);
+
+        setTasks(outTasks, keys, true);
+    }
+
+    loadTasksStorage()
 
     function transformObj(value) {
         const obj = []
@@ -60,12 +92,17 @@ function load() {
             obj.push(JSON.parse(eval(`localStorage.${values}`)))
         }
 
-        for(let i of obj) {
+        for (let i of obj) {
             allTasks.push(i)
         }
 
         return obj
     }
+
+    //Storage
+
+
+    //btn de mais opções
 
     function moreOptions() {
         document.querySelector('nav').classList.toggle('nav')
@@ -76,26 +113,60 @@ function load() {
         } else {
             imgInput[4].setAttribute('src', './assets/icon/more-options.svg')
         }
+    };
+
+    //btn de mais opções
+
+    //ouvidores para as tasks
+
+    function setEventListener() {
+        tasks = outTasks.querySelectorAll('.tasks')
+
+        for (let task of tasks) {
+            task.addEventListener('click', openTask)
+        }
     }
 
-    function loadTasksStorage() {
-        const len = localStorage.length
-
-        if (!len) return
-
-        const keys = Object.keys(localStorage);
-
-        setTasks(outTasks, keys, true)
+    function openTask() {
+        const tar = this
+        const task = container.querySelector('.task')
+        task.innerHTML = '<button class="btn">Voltar</button>'
+        const btn = container.querySelector('.btn')
+        btn.addEventListener('click', closeTask)
+        task.appendChild(tar)
+        tar.setAttribute(`style`, `overflow: auto;
+            cursor: auto; 
+            height: 85vh; 
+            margin: 1em;
+        `)
+        
+        removeOrAddDisplay(outTasks, true)
+        removeOrAddDisplay(task, false)
     }
 
-    loadTasksStorage()
+    function removeOrAddDisplay(body, value) {
+        if (value) return body.style.display = 'none'
+        body.style.display = 'grid'
+    }
 
-    form.addEventListener('submit', newTask)
+    function closeTask() {
+        const task = container.querySelector('.task')
+        setTasks(outTasks, allTasks)
+        removeOrAddDisplay(task, true)
+        removeOrAddDisplay(outTasks, false)
+    }
 
+    //ouvidores para as tasks
+
+    //ouvidores
+
+    form.addEventListener('submit', newTask);
     document.addEventListener('click', (e) => {
         if (e.target === imgInput[0] || e.target === liInput[0]) newNote()
         if (e.target === imgInput[4]) moreOptions()
     })
+
+    //ouvidores 
 
 }
 
